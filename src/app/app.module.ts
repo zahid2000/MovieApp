dac391c8ccb3a101671c94e523a28ec317357d11
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-08-05 17:09:43 +0400

    Lesson 11: Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MovieComponent } from './components/movie/movie.component';
 import { FormsModule } from '@angular/forms';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { LoggingComponent } from './components/logging/logging.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { LoggingComponent } from './components/logging/logging.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
