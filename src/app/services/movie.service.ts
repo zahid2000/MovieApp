87ae04e5bf960139c442a857aed3fe2540653c04
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-08-05 16:38:27 +0400

    Lesson 9.1: Observable

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Movie } from '../models/classes/movie';
 import { Movies } from '../models/classes/movie.datasource';
-
+import { Observable,of } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
   constructor() {}
 
-  getMovies(): Movie[] {
-    return Movies;
+  getMovies():Observable<Movie[]> {
+    return of(Movies);
   }
 }
