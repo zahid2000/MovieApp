92f6e101f19c5350a9bd809e3509d00588300f4d
Zahid Mamedov <zahidmamedov07@gmail.com>
2022-08-05 11:16:41 +0400

    Lesson 2: Genereting New Component

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:'app-movies',
+    selector:'movies',
     templateUrl:'movies.component.html',
     styleUrls:['movies.component.css']
     
